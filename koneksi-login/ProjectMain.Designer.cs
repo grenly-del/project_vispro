ad6dc7ba6a606c417fbe1b00bdf14b3b51896234
granly-del <ardiand400@gmail.com>
2024-09-23 20:11:48 +0800

    embuat database, dan melakukan CRUD

---
+++ koneksi-login/ProjectMain.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace koneksi_login
+{
+    partial class Project_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.Label();
+            this.txtLevel = new System.Windows.Forms.Label();
+            this.id = new System.Windows.Forms.TextBox();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.nama = new System.Windows.Forms.TextBox();
+            this.level = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.AutoSize = true;
+            this.txtID.Location = new System.Drawing.Point(106, 65);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(70, 13);
+            this.txtID.TabIndex = 0;
+            this.txtID.Text = "ID Pengguna";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.AutoSize = true;
+            this.txtUsername.Location = new System.Drawing.Point(106, 91);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(55, 13);
+            this.txtUsername.TabIndex = 1;
+            this.txtUsername.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AutoSize = true;
+            this.txtPassword.Location = new System.Drawing.Point(106, 116);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(53, 13);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "Password";
+            // 
+            // txtNama
+            // 
+            this.txtNama.AutoSize = true;
+            this.txtNama.Location = new System.Drawing.Point(106, 144);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(87, 13);
+            this.txtNama.TabIndex = 3;
+            this.txtNama.Text = "Nama Pengguna";
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.AutoSize = true;
+            this.txtLevel.Location = new System.Drawing.Point(106, 171);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(33, 13);
+            this.txtLevel.TabIndex = 4;
+            this.txtLevel.Text = "Level";
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(239, 62);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(63, 20);
+            this.id.TabIndex = 5;
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(239, 88);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(144, 20);
+            this.username.TabIndex = 6;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(239, 113);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(144, 20);
+            this.password.TabIndex = 7;
+            // 
+            // nama
+            // 
+            this.nama.Location = new System.Drawing.Point(239, 137);
+            this.nama.Name = "nama";
+            this.nama.Size = new System.Drawing.Size(144, 20);
+            this.nama.TabIndex = 8;
+            // 
+            // level
+            // 
+            this.level.FormattingEnabled = true;
+            this.level.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.level.Location = new System.Drawing.Point(239, 163);
+            this.level.Name = "level";
+            this.level.Size = new System.Drawing.Size(63, 21);
+            this.level.TabIndex = 9;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(478, 60);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(603, 59);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(478, 91);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(603, 91);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(478, 121);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(603, 120);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(75, 23);
+            this.btnLogout.TabIndex = 15;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(109, 276);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(569, 238);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Project_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 578);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.level);
+            this.Controls.Add(this.nama);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.id);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtID);
+            this.Name = "Project_Main";
+            this.Text = "Project_Main";
+            this.Load += new System.EventHandler(this.Project_Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtID;
+        private System.Windows.Forms.Label txtUsername;
+        private System.Windows.Forms.Label txtPassword;
+        private System.Windows.Forms.Label txtNama;
+        private System.Windows.Forms.Label txtLevel;
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox nama;
+        private System.Windows.Forms.ComboBox level;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ koneksi-login/ProjectMain.cs
@@ -0,0 +1,256 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace koneksi_login
+{
+    public partial class Project_Main : Form
+    {
+        private MySqlConnection koneksi;
+        private MySqlDataAdapter adapter;
+        private MySqlCommand perintah;
+        private DataSet ds = new DataSet();
+        private string alamat, query;
+        public Project_Main()
+        {
+            alamat = "server=localhost; database=db_mahasiswa; username=root; password=;";
+            koneksi = new MySqlConnection(alamat);
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (username.Text != "")
+                {
+                    query = string.Format("select * from tb_mahasiswa where username = '{0}'", username.Text);
+                    ds.Clear();
+                    koneksi.Open();
+                    perintah = new MySqlCommand(query, koneksi);
+                    adapter = new MySqlDataAdapter(perintah);
+                    perintah.ExecuteNonQuery();
+                    adapter.Fill(ds);
+                    koneksi.Close();
+                    if (ds.Tables[0].Rows.Count > 0)
+                    {
+                        foreach (DataRow kolom in ds.Tables[0].Rows)
+                        {
+                            id.Text = kolom["id_pengguna"].ToString();
+                            password.Text = kolom["password"].ToString();
+                            nama.Text = kolom["nama_pengguna"].ToString();
+                            level.Text = kolom["level"].ToString();
+
+                        }
+                        username.Enabled = false;
+                        dataGridView1.DataSource = ds.Tables[0];
+                        btnSave.Enabled = false;
+                        btnUpdate.Enabled = true;
+                        btnDelete.Enabled = true;
+                        btnSearch.Enabled = false;
+                        btnClear.Enabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data Tidak Ada !!");
+                        Project_Main_Load(null, null);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Data Yang Anda Pilih Tidak Ada !!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (username.Text != "" && password.Text != "" && nama.Text != "")
+                {
+
+                    query = string.Format("insert into tb_mahasiswa  values ('{0}','{1}','{2}','{3}','{4}');", id.Text, username.Text, password.Text, nama.Text, level.Text);
+
+
+                    koneksi.Open();
+                    perintah = new MySqlCommand(query, koneksi);
+                    adapter = new MySqlDataAdapter(perintah);
+                    int res = perintah.ExecuteNonQuery();
+                    koneksi.Close();
+                    if (res == 1)
+                    {
+                        MessageBox.Show("Insert Data Suksess ...");
+                        Project_Main_Load(null, null);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Gagal inser Data . . . ");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Data Tidak lengkap !!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (password.Text != "" && nama.Text != "" && username.Text != "" && id.Text != "")
+                {
+
+                    query = string.Format("update tb_mahasiswa set password = '{0}', nama_pengguna = '{1}', level = '{2}' where id_pengguna = '{3}'", password.Text, nama.Text, level.Text, id.Text);
+
+
+                    koneksi.Open();
+                    perintah = new MySqlCommand(query, koneksi);
+                    adapter = new MySqlDataAdapter(perintah);
+                    int res = perintah.ExecuteNonQuery();
+                    koneksi.Close();
+                    if (res == 1)
+                    {
+                        MessageBox.Show("Update Data Suksess ...");
+                        Project_Main_Load(null, null);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Gagal Update Data . . . ");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Data Tidak lengkap !!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtID.Text != "")
+                {
+                    if (MessageBox.Show("Anda Yakin Menghapus Data Ini ??", "Warning", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        query = string.Format("Delete from tb_mahasiswa where id_pengguna = '{0}'", id.Text);
+                        ds.Clear();
+                        koneksi.Open();
+                        perintah = new MySqlCommand(query, koneksi);
+                        adapter = new MySqlDataAdapter(perintah);
+                        int res = perintah.ExecuteNonQuery();
+                        koneksi.Close();
+                        if (res == 1)
+                        {
+                            MessageBox.Show("Delete Data Suksess ...");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Gagal Delete data");
+                        }
+                    }
+                    Project_Main_Load(null, null);
+                }
+                else
+                {
+                    MessageBox.Show("Data Yang Anda Pilih Tidak Ada !!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Project_Main_Load(null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+
+        private void Project_Main_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                koneksi.Open();
+                query = string.Format("select * from tb_mahasiswa");
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                ds.Clear();
+                adapter.Fill(ds);
+                koneksi.Close();
+                dataGridView1.DataSource = ds.Tables[0];
+                dataGridView1.Columns[0].Width = 100;
+                dataGridView1.Columns[0].HeaderText = "ID Pengguna";
+                dataGridView1.Columns[1].Width = 150;
+                dataGridView1.Columns[1].HeaderText = "Username";
+                dataGridView1.Columns[2].Width = 120;
+                dataGridView1.Columns[2].HeaderText = "Password";
+                dataGridView1.Columns[3].Width = 120;
+                dataGridView1.Columns[3].HeaderText = "Nama Pengguna";
+                dataGridView1.Columns[4].Width = 120;
+                dataGridView1.Columns[4].HeaderText = "Level";
+
+                id.Clear();
+                nama.Clear();
+                password.Clear();
+                username.Clear();
+                id.Focus();
+                btnUpdate.Enabled = false;
+                btnDelete.Enabled = false;
+                btnClear.Enabled = false;
+                btnSave.Enabled = true;
+                btnSearch.Enabled = true;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
